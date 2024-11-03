4af900251791c0e7350046921cafbaa2dd8e6c5a
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-03 23:56:34 +0530

    Init

---
+++ Inheritance/src/StringPractise/RevArray.java
@@ -0,0 +1,22 @@
+package StringPractise;
+
+public class RevArray {
+
+	public static void main(String[] args) {
+		System.out.println(reverse("Shrabana"));
+
+	}
+      static String reverse(String s) {
+          char[]a =s.toCharArray();
+          int i=0;
+          int j = a.length-1;
+          while(i<j) {
+          char temp = a[i];
+                   a[i]=a[j];
+                   a[j]=temp;
+                   i++;
+                   j--;
+      }
+      return new String(a);
+}
+}
