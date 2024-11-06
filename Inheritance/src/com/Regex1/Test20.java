00eb5c4dfa7be1d32d38ba3a467fc87d683a49be
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-06 23:00:01 +0530

    Init

---
+++ Inheritance/src/com/Regex1/Test20.java
@@ -0,0 +1,24 @@
+package com.Regex1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Test20 {
+
+	public static void main(String[] args) {
+		String exp = "([a-z])\\1*";
+		String str ="success";
+		Pattern p = Pattern.compile(exp);
+		Matcher m = p.matcher(str);
+		
+		
+		String  s2 = " ";
+		while(m.find()){
+			String s1 = m.group();
+			s2=s2+s1.length()+s1.substring(0,1);
+			}
+		System.out.println(s2);
+
+	}
+
+}
