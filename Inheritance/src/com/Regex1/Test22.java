4629b3bec7b5090a6653bb8539b88f44e0ba1ff6
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-06 23:26:21 +0530

    Init

--- Inheritance/src/com/Regex1/Test22.java
+++ Inheritance/src/com/Regex1/Test22.java
@@ -3,18 +3,15 @@ package com.Regex1;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Test21 {
+public class Test22 {
 
 	public static void main(String[] args) {
-		String exp = "([a-z]+)\\1"; //
-		String str = "banana";
+		String exp = "[0-9]{4}";
+		String str = "3456";
+		
 		Pattern p = Pattern.compile(exp);
 		Matcher m = p.matcher(str);
-		
-		while(m.find()) {
-			System.out.println(m.group()+" "+m.start()+" "+m.end());
-			
-		}
+		System.out.println(m.matches());
 
 	}
 
