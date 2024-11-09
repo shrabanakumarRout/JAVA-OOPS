37016a40e3f9648f3999d7a92f6bfedb3f30a42e
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-09 11:37:25 +0530

    Rotate only last Array to 1st Array

--- Corejava/src/com/JspArraypgms/Rotate.java
+++ Corejava/src/com/JspArraypgms/Rotate.java
@@ -5,19 +5,22 @@ import java.util.Arrays;
 public class Rotate {
 
 	public static void main(String[] args) {
-		int a[] = {1,2,3,4,5};
-		rotate(a);
-		System.out.println(Arrays.toString(a));
-
+	    int[]a= {1,2,3,4,5};
+	    rotate(a);
+	    System.out.print(Arrays.toString(a));
 	}
 	
-	static void rotate(int[]a) {
+	
+	
+	static void rotate(int []a) {
 		int len = a.length;
 		int temp = a[len-1];
+		
 		for(int i=len-2;i>=0;i--) {
 			a[i+1]=a[i];
 		}
 		a[0]=temp;
+		
 	}
 
 }
