09b6a5ff96d572572fa8b437acc58dbc75774c83
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-10 12:58:30 +0530

    Sorting

--- Corejava/src/com/JspArraypgms/SelectionSort.java
+++ Corejava/src/com/JspArraypgms/SelectionSort.java
@@ -6,22 +6,18 @@ public class SelectionSort {
 		int []a= {5,7,4,2,6,3,8,1};
 		System.out.println(Arrays.toString(a));
 		sort(a);
-		System.out.println(Arrays.toString(a));
-		
-		
+		System.out.println(Arrays.toString(a));		
 }
 	static void sort(int a[]) {
 		for(int i = 0;i<a.length-1;i++) {
-			int miniindex = i;
+			int minindex = i;
 			for(int j=i+1;j<a.length;j++) {
-				if(a[miniindex]>a[j]) {
-					miniindex = j;
-					
-				}
+				if(a[minindex]>a[j]) {
+					minindex = j;
+					  }
 			
-				
-				int temp = a[miniindex];
-				a[miniindex]=a[i];
+				int temp = a[minindex];
+				a[minindex]=a[i];
 				a[i]=temp;
 				
 			}
