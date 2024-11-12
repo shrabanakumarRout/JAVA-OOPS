3c58c44da31e0cded4dcdabfe6ce0287ef58c822
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-12 23:52:04 +0530

    SumofArray

---
+++ Corejava/src/TwoDimArray/Demo2.java
@@ -0,0 +1,20 @@
+package TwoDimArray;
+
+public class Demo2 {
+	public static void main (String []args) {
+		int [][] a= {
+				
+		
+				{1,2,3},
+				{4,5,6},
+				{7,8,9},
+		};
+		int len =a.length;
+		int sum = 0;
+		for(int i =0;i<len;i++) {
+			sum+=a[i][i];
+		}
+		System.out.println(sum);
+	}
+      
+}
