bf024fb60ad558af21df8b411a3794990b3b4c09
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-14 14:19:42 +0530

    Sort the Array using method

---
+++ Corejava/src/TwoDimArray/Test.java
@@ -0,0 +1,15 @@
+package TwoDimArray;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int [] a= {6,5,4,8,9,7,2};
+		
+		Arrays.sort(a); //method to sort the array from small to big
+		System.out.println(Arrays.toString(a));
+
+	}
+
+}
