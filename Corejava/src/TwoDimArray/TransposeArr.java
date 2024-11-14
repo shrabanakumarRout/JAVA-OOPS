2ebb2c5f1ccf6906faacd03aaba59ed71ab3a92f
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-14 13:56:00 +0530

    Transpose2dArray

---
+++ Corejava/src/TwoDimArray/TransposeArr.java
@@ -0,0 +1,28 @@
+package TwoDimArray;
+
+public class TransposeArr {                  /* 
+                                                   2 7 6 
+                                                   3 1 9 
+                                                   5 0 5    */
+
+	public static void main(String[] args) {
+		int a[][]= {
+				       {2,3,5},
+				       {7,1,0},
+				       {6,9,5}
+		     };
+                 int [][]b=new int[a.length][a.length];
+                 for(int i=0;i<a.length;i++) {
+                	 for(int j=0;j<a[i].length;j++) {
+                		 b[i][j]=a[j][i];
+                	 }
+                 }
+                 for(int[]n:b) {
+                	 for(int ar:n) {
+                		 System.out.print(ar+" ");
+                	 }
+                	 System.out.println();
+                 }
+	}
+
+}
