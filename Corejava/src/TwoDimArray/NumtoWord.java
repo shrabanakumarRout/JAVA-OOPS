f532a7e3f107fe96cc8218fb769a4d81bdd6da82
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-14 23:11:53 +0530

    Important for interview

---
+++ Corejava/src/TwoDimArray/NumtoWord.java
@@ -0,0 +1,31 @@
+package TwoDimArray;
+
+public class NumtoWord {
+
+	public static void main(String[] args) {
+		int num=24764345;
+		toword(num/10000000," crore");
+		toword((num / 100000)%100,"lakhs");
+        toword((num/1000)%100,"thousand");
+        toword((num/100)%10,"hundread");
+        toword(num%100,"");
+	}
+	
+	public static void toword(int num ,String s) {
+		String []one= {" ","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fiveteen","sixteen","seventeen","eighteen","nineteen"};
+		
+		String []two= {" "," ","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty"};
+		
+		if(num<=19) {
+			System.out.print(one[num]);
+		}
+		else {
+			System.out.print(two[num/10]+""+one[num%10]);
+		}
+		if(num!=0)
+		System.out.print(s);
+	}
+	
+	
+
+}
