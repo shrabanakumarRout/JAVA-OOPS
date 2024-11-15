4340623e81fbf208385303d7979ff58f5038dc80
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-15 18:43:30 +0530

    sorting the String type Array

---
+++ Corejava/src/TwoDimArray/Sorting.java
@@ -0,0 +1,18 @@
+package TwoDimArray;
+
+import java.util.Arrays;
+
+public class Sorting {
+
+	public static void main(String[] args) {
+		String[]a= {"mango","apple","orange","banana","guava"};
+		
+		Arrays.sort(a);
+		
+		for(String s:a) {
+			System.out.println(s);
+		}
+
+	}
+
+}
