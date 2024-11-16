e029b7c2ea9330485548a37a43db1413b47fade6
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-16 20:27:11 +0530

    DS Prgm1

---
+++ DataStructure/src/com/datastrucureprgms/ArrayTest.java
@@ -0,0 +1,11 @@
+package com.datastrucureprgms;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		Arraylist a = new Arraylist();
+		System.out.println(a.add(20));
+
+	}
+
+}
---
+++ DataStructure/src/com/datastrucureprgms/Arraylist.java
@@ -0,0 +1,24 @@
+package com.datastrucureprgms;
+
+public class Arraylist {
+	   private Object[]a;
+	   private int pos=0;
+	   public Arraylist() {
+		    a= new Object[5];
+		    pos=0;
+		   
+	   }
+	   
+	   public String add(Object o) {
+		   if(pos>=a.length) {
+			   Object[]temp=new Object[a.length+3];
+			   System.arraycopy(a, 0, temp, 0, a.length);
+			   a=temp;
+		   }
+		   a[pos]=o;
+		   pos++;
+		   return "Added SuccessFully " +o;
+	   }
+	  
+
+}
