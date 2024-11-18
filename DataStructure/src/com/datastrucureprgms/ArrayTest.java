8008e664589d5dc48ea2a03d3ca22eddf1641faf
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-18 23:06:08 +0530

    Arraylist

--- DataStructure/src/com/datastrucureprgms/ArrayTest.java
+++ DataStructure/src/com/datastrucureprgms/ArrayTest.java
@@ -3,9 +3,26 @@ package com.datastrucureprgms;
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		Arraylist a = new Arraylist();
-		System.out.println(a.add(20));
-
+		Arraylist l = new Arraylist();
+		l.add("Shrabana Kumar Rout");
+        l.add("Subhangi Baliyarsingh");
+        l.add("Soumitra jena");
+        l.add("Swadhin Kumar Rout");
+        l.add("Santunu Barik");
+        l.add("Satyaprajna Bimlesh");
+        //l.adding(2,70);
+        
+     // System.out.println(l.remove(2));
+      
+      for(int i=0;i<l.size();i++) {
+    	  System.out.println((String)l.get(i));
+      }
+     l.remove(3);
+     System.out.println("----------");
+     for(int i=0;i<l.size();i++) {
+    	 System.out.println((String)l.get(i));
+     }
 	}
+	
 
 }
--- DataStructure/src/com/datastrucureprgms/Arraylist.java
+++ DataStructure/src/com/datastrucureprgms/Arraylist.java
@@ -3,22 +3,60 @@ package com.datastrucureprgms;
 public class Arraylist {
 	   private Object[]a;
 	   private int pos=0;
+	   
+	   
 	   public Arraylist() {
 		    a= new Object[5];
 		    pos=0;
 		   
 	   }
 	   
-	   public String add(Object o) {
-		   if(pos>=a.length) {
-			   Object[]temp=new Object[a.length+3];
-			   System.arraycopy(a, 0, temp, 0, a.length);
-			   a=temp;
-		   }
-		   a[pos++]=o;
-		   
-		   return "Added SuccessFully " +o;
+	   public void add(Object e) {
+		   if(pos>=a.length)increase();
+			   a[pos++]=e;  
+		  
 	   }
-	  
-
+	   public void increase() {
+		   Object[]temp=new Object[a.length+3];
+		   System.arraycopy(a, 0, temp, 0, a.length);
+		   a=temp;
+	   }
+	   
+	   public int size(){
+		   return pos;
+	   }
+	   
+	   
+	   public Object get(int index) {
+		   if(index<=-1 || index>=size()) {
+			   IndexOutOfBoundsException e =  new IndexOutOfBoundsException();
+			   throw e;
+		   }
+		   return a[index];
+		   }
+	   
+	   public void adding(Object e,int index) {
+		   if(index<=-1 || index>=size() ) {
+			 throw   new IndexOutOfBoundsException();
+		   }
+		   if(pos>=a.length) increase();
+		   for(int i=size()-1;i>=size();i--) {
+			   a[i+1]=a[i];
+		   }
+		   a[index]=e;
+		   pos++;
+	
+	   }
+	   
+	   public void remove(int index) {
+		   if(index<=-1 || index>=size()) {
+			  throw   new IndexOutOfBoundsException();
+		   }
+		   for(int i=index+1;i<size();i++) {
+			   a[i-1] =a[i];
+		   }
+		    pos--;
+		    a[pos]=null;
+		   
+	   }
 }
