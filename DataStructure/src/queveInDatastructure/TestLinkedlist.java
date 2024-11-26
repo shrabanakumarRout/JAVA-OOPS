8e333e4bdd8c64ba5995415235069b7c46074999
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-26 20:59:24 +0530

    Linkedlist

--- DataStructure/src/queveInDatastructure/TestLinkedlist.java
+++ DataStructure/src/queveInDatastructure/TestLinkedlist.java
@@ -4,11 +4,11 @@ public class TestLinkedlist {
 
 	public static void main(String[] args) {
 		Linkedlist l = new Linkedlist();
-		l.add("Simpul");
-		l.add("Pinu");
-		l.add("Subhangi");
-		l.add("Santunu");
-		l.add("Papul");
+		l.push("Simpul");
+		l.push("Pinu");
+		l.push("Subhangi");
+		l.push("Santunu");
+		l.push("Papul");
 		
 		System.out.println(l.size());
 		System.out.println(l.poll());
