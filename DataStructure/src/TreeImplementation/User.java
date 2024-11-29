c7c8243573ed47474b16088824b18cf670ed761b
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-11-29 22:05:59 +0530

    Update user.java

--- DataStructure/src/TreeImplementation/User.java
+++ DataStructure/src/TreeImplementation/User.java
@@ -3,7 +3,18 @@ package TreeImplementation;
 public class User {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		binarysearchtree b = new binarysearchtree();
+		
+		b.add(50);
+		b.add(20);
+		b.add(70);
+		b.add(40);
+		b.add(60);
+		b.add(80);
+		
+		
+		
+		
 
 	}
 
