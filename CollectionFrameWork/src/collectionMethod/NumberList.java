3443c3d6897a8b87a22bfa18100739a2553f889b
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-05 20:53:43 +0530

    Using CollectionMethods

---
+++ CollectionFrameWork/src/collectionMethod/NumberList.java
@@ -0,0 +1,27 @@
+package collectionMethod;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NumberList {
+
+	public static void main(String[] args) {
+		List l = new ArrayList();
+		
+		l.add(80);
+		l.add(40);
+		l.add(20);
+		l.add(30);
+		l.add(50);
+		
+		System.out.println(l);  //Before Sort
+		Collections.sort(l);
+		System.out.println(l);  //After Sort
+		System.out.println(l.getFirst());
+		System.out.println(l.getLast());
+
+	}
+
+}
