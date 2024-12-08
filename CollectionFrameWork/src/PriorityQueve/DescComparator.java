0c543582e2d6f1373632e677cb7a8a474d283ea4
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-08 23:50:03 +0530

    Priority Queve

---
+++ CollectionFrameWork/src/PriorityQueve/DescComparator.java
@@ -0,0 +1,15 @@
+package PriorityQueve;
+
+import java.util.Comparator;
+
+public class DescComparator implements Comparator<Integer>{
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		return o2-o1;
+	}
+
+	
+	
+}
