7fba44590e847164ae598b8078e95ba92269e381
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-14 18:35:08 +0530

    Stock Class Created for DeadLock Method

--- Corejava/src/com/multithreading/Stock.java
+++ Corejava/src/com/multithreading/Stock.java
@@ -15,17 +15,24 @@ public class Stock {
 		
 	}
 	
-	public void update(int n) {
+	synchronized public void update(int n) {
 		items = items+n;
 		System.out.println("Stock is Updated to "+items+" Stock");
+		notifyAll();
 	}
-	public void Consume(int n) {
+	
+	
+	
+	synchronized public void Consume(int n) {
 		System.out.println("Consumers Requiers "+ n +" items");
 		while(items<n) {
 			System.out.println("Waiting for Update");
+		
+		try {wait();} catch(Exception e) {e.printStackTrace();}
 		}
 		items-=n;
 		System.out.println(n+"items are Consumed");
 	}
-  
 }
+	
+
