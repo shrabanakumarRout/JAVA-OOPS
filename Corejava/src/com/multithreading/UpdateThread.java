80641750e1599c07696c9b94f062a258c4aae958
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-14 18:34:09 +0530

    Update the stocks Using  runnable interface

--- Corejava/src/com/multithreading/UpdateThread.java
+++ Corejava/src/com/multithreading/UpdateThread.java
@@ -3,14 +3,14 @@ package com.multithreading;
 public class UpdateThread implements Runnable{
 	int items;
 	
-	
-
+	public UpdateThread(int items) {
+		this.items=items;
+		new Thread(this).start();
+	}
 	@Override
 	public void run() {
+		System.out.println("Update Thread Starts");
+		Stock.getInstance().update(items);
 		
-		
-	}
-
-
-	
+	}	
 }
