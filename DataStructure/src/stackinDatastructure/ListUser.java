971c898d0203a6b25977b8705a6ef7a791dc4aae
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-15 23:30:17 +0530

    ListUser

---
+++ DataStructure/src/stackinDatastructure/ListUser.java
@@ -0,0 +1,22 @@
+package stackinDatastructure;
+
+public class ListUser {
+
+	public static void main(String[] args) {
+		ArrayList a =new ArrayList();
+		
+		 a.push("Shrabana Rout");
+		 a.push("Swadhin  Rout");
+		 a.push("Subhangi BaliarSingh");
+		 
+		 System.out.println("Top element: " + a.peek());
+		 
+		 System.out.println("Current stack size: " + a.size());
+		 
+		 
+		 System.out.println("Stack elements:");
+	        a.get();
+
+	}
+
+}
