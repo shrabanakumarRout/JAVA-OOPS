9829b9db2a96c31b32d0492f2157b14f412ca65a
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-16 19:32:26 +0530

    Get the result from the Interface

---
+++ CollectionFrameWork/src/cfMapInterface/Test3.java
@@ -0,0 +1,32 @@
+package cfMapInterface;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		Map<Integer,String> m = new TreeMap<Integer,String>();
+		
+		m.put(7,"Thala");
+		m.put(18,"Kohli");
+		m.put(45,"Hitman");
+		m.put(93,"Bumrah");
+		m.put(1,"KL Rahul");
+		
+		Set<Integer> s = m.keySet();
+		
+		Iterator <Integer> i = s.iterator();
+		while(i.hasNext()) {
+			int key=i.next();
+			String res = m.get(key);
+		
+			System.out.println(key + " "+ res);	
+			}
+		}
+	}
+
+
