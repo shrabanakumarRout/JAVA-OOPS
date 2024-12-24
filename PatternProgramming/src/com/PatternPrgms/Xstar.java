d7c7d989cba86e1a3ef59cfb9f29ea0711b136cf
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-24 12:29:12 +0530

    Xstar

---
+++ PatternProgramming/src/com/PatternPrgms/Xstar.java
@@ -0,0 +1,30 @@
+package com.PatternPrgms;
+
+public class Xstar {
+
+	public static void main(String[] args) {
+		int n=7;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				if(i==j || i+j==n+1) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
+
+
+//  *     *
+//   *   * 
+//    * *  
+//     *   
+//    * *  
+//   *   * 
+//  *     *
