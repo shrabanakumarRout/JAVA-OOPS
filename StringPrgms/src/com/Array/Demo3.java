750f5b1ea9e91502946dccefde7b334b01489472
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-25 22:35:32 +0530

    Increment the value

---
+++ StringPrgms/src/com/Array/Demo3.java
@@ -0,0 +1,24 @@
+package com.Array;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		int []a= {10,20,30,40};
+		m1(a);
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+	}
+	     static void  m1(int[]a) {
+			for(int i=0;i<a.length;i++) {
+				a[i]++;
+			}
+			System.out.println("m1 completed");
+		
+
+	
+	}
+
+	
+
+}
