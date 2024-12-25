2b091274239e7088bf6e49860bb68db3f147c4be
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-25 22:34:28 +0530

    Using For each loop

---
+++ StringPrgms/src/com/Array/Demo1.java
@@ -0,0 +1,18 @@
+package com.Array;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int []a=new int[4];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		
+		for (int n:a) {
+			System.out.println(n);
+		}
+
+	}
+
+}
