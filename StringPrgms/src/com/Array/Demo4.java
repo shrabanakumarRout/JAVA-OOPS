4709fd49fcd12389952d172ebb4e2a140df28699
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-25 22:35:58 +0530

    Print Using Array Method

---
+++ StringPrgms/src/com/Array/Demo4.java
@@ -0,0 +1,12 @@
+package com.Array;
+
+import java.util.Arrays;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		int []a= {1,2,3,4};
+		System.out.println(Arrays.toString(a));
+	}
+
+}
