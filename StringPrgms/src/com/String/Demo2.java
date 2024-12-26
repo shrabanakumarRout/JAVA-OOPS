1b5b0c6e4106f668bcf27a4c5751a0e64888ce97
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-26 22:00:32 +0530

    Basic

---
+++ StringPrgms/src/com/String/Demo2.java
@@ -0,0 +1,14 @@
+package com.String;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		String s1= "java";
+		String s2 = "java";
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));
+		System.out.println(s1 == s2);
+	}
+
+}
