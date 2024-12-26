66084e5299f52ed1b919764ca8de63326662dab0
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-26 22:00:51 +0530

    Basic

---
+++ StringPrgms/src/com/String/Demo4.java
@@ -0,0 +1,12 @@
+package com.String;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		String s1 = "javaDeveloper";
+		System.out.println(s1.substring(4));
+		System.out.println(s1.substring(4, 7));
+
+	}
+
+}
