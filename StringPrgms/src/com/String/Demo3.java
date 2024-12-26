a5f3061280c8146321bc997b29185fc5bc2bf010
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-26 22:08:29 +0530

    Update

--- StringPrgms/src/com/String/Demo3.java
+++ StringPrgms/src/com/String/Demo3.java
@@ -5,12 +5,15 @@ public class Demo3 {
 	public static void main(String[] args) {
 		String s="java";
 		
-		for(int i=0;i<=s.length();i++) {
-			System.out.print(s.charAt(i));
+		for(int i=0;i< s.length();i++) {
+			System.out.println(s.charAt(i));
 		}
-		System.out.print("------------------------------");
-		for(int i=s.length()-1;i>=0;i--) {
-			System.out.print(s.charAt(i));
+		
+		System.out.println("------------------------------");
+		for(int i=s.length()-1;i>=0;i--){
+			
+			System.out.println(s.charAt(i));
+			
 		}
 
 	}
