e1678dd413b23c24c92087c174e2573615ceddee
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-27 21:56:38 +0530

    Perfect

---
+++ StringPrgms/src/com/Basics/Perfect.java
@@ -0,0 +1,22 @@
+package com.Basics;
+
+public class Perfect {
+
+	public static void main(String[] args) {
+		int n = 22;
+		int sum = 0;
+		for(int i=1;i<=n/2;i++) {
+			if(n%i==0) {
+				sum+=i;
+			}
+			}
+		if(n==sum) {
+			System.out.println(n+ " "+"is a perfect number");
+		}
+		else {
+			System.out.println(n+ " "+ "is not a perfect number");
+		}
+
+	}
+
+}
