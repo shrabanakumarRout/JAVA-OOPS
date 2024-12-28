fff072b3c2a0feb6d07d8540bf64a1f8081da5c2
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-28 22:16:13 +0530

    Update

--- StringPrgms/src/com/Basics/Perfect.java
+++ StringPrgms/src/com/Basics/Perfect.java
@@ -3,7 +3,7 @@ package com.Basics;
 public class Perfect {
 
 	public static void main(String[] args) {
-		int n = 22;
+		int n = 22; //Variable Declaration
 		int sum = 0;
 		for(int i=1;i<=n/2;i++) {
 			if(n%i==0) {
