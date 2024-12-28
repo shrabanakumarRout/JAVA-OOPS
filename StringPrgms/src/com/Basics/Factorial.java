b92e567d70c501a62947322a3958cf2e0f1e5dfe
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-28 22:15:28 +0530

    Update

--- StringPrgms/src/com/Basics/Factorial.java
+++ StringPrgms/src/com/Basics/Factorial.java
@@ -3,7 +3,7 @@ package com.Basics;
 public class Factorial {
 
 	public static void main(String[] args) {
-		int n = 5;
+		int n = 5; //Variable Declaration
 		int fact = 1;
 		for(int i=1;i<=n;i++) {
 			fact*=i;
