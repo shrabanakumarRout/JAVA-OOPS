749d485d91d092562807e7d31e5c78355074a974
shrabanakumarRout <121253249+shrabanakumarRout@users.noreply.github.com>
2024-12-28 22:15:51 +0530

    Update

--- StringPrgms/src/com/Basics/Hello.java
+++ StringPrgms/src/com/Basics/Hello.java
@@ -3,7 +3,7 @@ package com.Basics;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World");//Print HelloWorld
 	}
 
 }
